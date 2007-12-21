fbd151263b8657b94d11d06b22c052643961ff38
mh <mh@immerda.ch>
2007-12-21 15:33:35 +0000

    refactored some stuff out in a modules

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+# modules/rkhunter/manifests/init.pp - manage rkhunter
+# Copyright (C) 2007 admin@immerda.ch
+# 
+
+modules_dir { "cft": }
+
+class cft {
+        package { 'gamin':
+                ensure => present,
+        }
+        package { 'ruby-fam':
+                ensure => present,
+                require => Package[gamin],
+        }
+        package { 'cft':
+                ensure => present,
+                require => Package[ruby-fam],
+        }
+}
+
