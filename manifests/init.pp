13209d32ff527187bb9739e445494011fecb2a43
mh <mh@immerda.ch>
2009-09-29 01:08:20 +0200

    codestyle

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,19 +2,6 @@
 # Copyright (C) 2007 admin@immerda.ch
 # 
 
-#modules_dir { "cft": }
-
 class cft {
-        package { 'gamin':
-                ensure => present,
-        }
-        package { 'ruby-fam':
-                ensure => present,
-                require => Package[gamin],
-        }
-        package { 'cft':
-                ensure => present,
-                require => Package[ruby-fam],
-        }
+  include cft::base
 }
-
